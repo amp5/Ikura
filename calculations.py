1c9c63d0962871607bc69e84ada7ef98852ceaf2
Alexandra Plassaras <aplassaras@gmail.com>
2015-05-27 18:06:35 -0700

    Working code that can handle multiple cards. woot.

--- calculations.py
+++ calculations.py
@@ -23,33 +23,23 @@ def min_payment_plan(name, date, debt, apr, user_id):
 
 	while debt >0:
 
-		interest_to_pay = debt * interest_per_month
+		interest_to_pay = round((debt * interest_per_month), 2)
 		interest_to_pay_list.append(interest_to_pay)
 
-		min_total_payment = min_payment + interest_to_pay
+		min_total_payment = round((min_payment + interest_to_pay) ,2)
 		min_total_payment_list.append(min_total_payment)
 
-		new_debt = debt + interest_to_pay - min_total_payment
+		new_debt = round((debt + interest_to_pay - min_total_payment), 2)
 		new_debt_list.append(new_debt)
 		debt = new_debt
 
-	# print "Min Debt decreasing:", new_debt_list
-	# print "*"* 20 
-	# print "Min Interest decreasing", interest_to_pay_list
-	# print "*"* 20 
-	# print "Min payments", min_total_payment_list
-
 	print "*"* 40
 
-	minimum = {"Minimum":[new_debt_list, interest_to_pay_list, min_total_payment_list]}
+	
 
+	minimum = {"Minimum":[new_debt_list, interest_to_pay_list, min_total_payment_list]}
 	card[name] = minimum
 
-	# print "This is card", card
-
-	# print "This is how long debt list is", len(new_debt_list)
-	# print '--'*20
-	# print "This is dictionary cards", card 
 	return card
 
 
@@ -60,15 +50,11 @@ def suggested_plan(name, date, debt, apr, card, user_id):
 	{Name: [new_debt_list(min), interest_to_pay_list(min), min_total_payment_list, 
 			new_debt_list(suggested), interest_to_pay_list(suggested), monthly_payment_list])}
 	"""
-	# print "These are my card", card
 
 	interest_per_month = apr/date
-	monthly_payment_suggestion = float(debt) / float((date + 1))
+	monthly_payment_suggestion = round(float(debt) / float((date + 1)), 2)
 	rounded_monthly_payment_suggestion = round(monthly_payment_suggestion, 2)
 
-	# minimum = card[name]
-
-	minimum = card[name].items()
 
 	# ----- # Suggested payment calculations # ----- # 
 
@@ -77,48 +63,26 @@ def suggested_plan(name, date, debt, apr, card, user_id):
 	monthly_payment_list = []
 
 	while debt >0:
-		interest_to_pay = debt * interest_per_month
+		interest_to_pay = round((debt * interest_per_month), 2)
 		interest_to_pay_list.append(interest_to_pay)
 
 		monthly_payment_list.append(rounded_monthly_payment_suggestion)
 
-		new_debt = debt + interest_to_pay - rounded_monthly_payment_suggestion
+		new_debt = round((debt + interest_to_pay - rounded_monthly_payment_suggestion), 2)
 		new_debt_list.append(new_debt)
 		debt = new_debt
 
-	# print "This is my card dict so far......", card
+
+	minimum = card[name].values()
+	minimum = minimum[0]
+
+	card[name] = {"Suggested": [new_debt_list, interest_to_pay_list, monthly_payment_list], "Minimum" : minimum}
 
 
-	suggested = {"Suggested": [new_debt_list, interest_to_pay_list, monthly_payment_list]}
+	dict_within_user = card.values()[0]
+	sugg_value_from_dict = dict_within_user["Suggested"]
+	min_value_from_dict = dict_within_user["Minimum"]
 
-# ***************************************************************************************************
-# TRYING TO ADD THIS DICTIONARY TO THE DICTIONARY INSIDE CARD {VISA: {MIN : [....]}, {SUGG : [....]}}
-# ***************************************************************************************************
-	# this is my minimum dictionary but I can't seem to add the suggested dictionary to this...
-	minimum = card[name].items()
-    
-	print "What is, ", minimum.append(suggested)
-	# print "What is this now", card
-
-
-	# # print "This is suggested dict", suggested
-	# values = card.values
-	# print "This should be values of card", values
-
-	# total_card = card.update(suggested)
-	# print "This should have min and sugg values for card", total_card
-
-# 	card[name].append([new_debt_list, interest_to_pay_list, monthly_payment_list])
-
-
-# 	card[name].value.update(extra)
-
-
-
-# card[name] = {"Minimum":[new_debt_list, interest_to_pay_list, min_total_payment_list]}
-
-	# print '--'*20
-	# print "This is dictionary card", card
 
 	return card 
 
@@ -129,13 +93,6 @@ def user_cards(query_results):
 	The inner dictionary key = name of a card, values = min payments, min intr rates,
 														min debt decrease, suggested payments,
 														suggested intr rates, suggested debt decrease """
-
-
-
-	#TODO: #
-	# Make sure to take user_id from session once it's connected #
-	# for now just manually entering in where user_id is 1. AKA the only user in the database
-
 	
 	user_dict = {}
 	card_dict_list = []
@@ -144,7 +101,6 @@ def user_cards(query_results):
 		name = card.card_name 
 		debt = card.card_debt
 		apr = card.card_apr
-
 		#This will turn user inputted apr into percentage
 		apr = apr/100
 		date = card.card_date
@@ -161,10 +117,9 @@ def user_cards(query_results):
 
 		returned_dict = min_payment_plan(name, date, debt, apr, user_id)
 		completed_card_dict = suggested_plan(name, date, debt, apr, returned_dict, user_id)
-		
 		card_dict_list.append(completed_card_dict)
 
-	# print "This is card list", card_dict_list
+
 
 	user_dict = {user_id : {}}
 	
@@ -176,6 +131,13 @@ def user_cards(query_results):
 	# print "Complete User Dictionary:", user_dict
 
 	return user_dict
+
+
+
+
+#TODO: #
+# Make sure to take user_id from session once it's connected #
+# for now just manually entering in where user_id is 1. AKA the only user in the database
 
 
 # TODO: # 
--- organize_calcs.py
+++ organize_calcs.py
@@ -10,156 +10,188 @@ from datetime import datetime
 # connect_to_db(app)
 
 
-#  This should be a dict with key of card name, each value is a diction with total min and sugg
+#  This should be a dict with key of card name, each value is a dictionary key value pair of min and sugg
 
 def organization(dictionary):
 	"""This is rearranging my data to display on html page and for D3"""
 
 	user_card_dict_py = dictionary
+	# print "What is this?", user_card_dict_py
 	values = user_card_dict_py.values()
 	# print "All of values", type(values)
 	# print "First index within values", type(values[0])
 	# print "First index within idex within values", type(values[0][0])
+	# print "What is this? The first card dict?", values[0][0]
 
-	total_min_debt = []
-	total_min_int = []
-	total_min_payment = []
+	min_debt_list = []
+	min_int_list = []
+	min_payment_list = []
 
-	total_sugg_debt = []
-	total_sugg_int = []
-	total_sugg_payment = []
+	sugg_debt_list = []
+	sugg_int_list = []
+	sugg_payment_list = []
 
 	# print "What is", values[0]
 
 	for i in range(len(values[0])):
 		print "*" * 100
 		card = values[0][i]
-		# print "This is the dictionary of my card:", card
-
-		card_values = card.values()
-		# print "These are the values of my card:", card_values
-
+		# print  "Here is a card!", card
+		dict_of_payments_per_card = card.values()[0]
+		sugg_payment_info = dict_of_payments_per_card["Suggested"]
+		min_payment_info = dict_of_payments_per_card["Minimum"]
+		# print "This is min payment info", min_payment_info
 
 	# *************************************
 		# Minimum Payment Data #
 	# *************************************
 
-		min_info = card_values[0][0]
-		# print "This is all of the info related to my card's minimum payment:", min_info
-
-		min_debt = min_info[0]
+		min_debt = min_payment_info[0]
 		# print "This is minimum debt on my card as it decreases:", min_debt
-		total_min_debt.append(min_debt)
-		# Should be a list of lists... YES
-		# print "This is my total minimum debt decreasing.", total_min_debt
-		# print "Type", type(total_min_debt)
+		min_debt_list.append(min_debt)
 
-		min_int = min_info[1]
+
+		min_int = min_payment_info[1]
 		# print "This is the minimum interest accruing for each payment:", min_int
-		total_min_int.append(min_int)
-		# print "This is my total minimum interest", total_min_int
-		# print "Type", type(total_min_int)
+		min_int_list.append(min_int)
 
-		min_payment = min_info[2]
+
+		min_payment = min_payment_info[2]
 		# print "This is the minimum payments for card until debt is gone:", min_payment
-		total_min_payment.append(min_payment)
-		# print "Total min payment", total_min_payment
+		min_payment_list.append(min_payment)
 
 	# *************************************
 		# Suggested Payment Data # 
 	# *************************************
-		sugg_info = card_values[0][1]
-		# print "This is all of the info related to my card's suggested payment", sugg_info
 
-		sugg_debt = sugg_info[0]
+		sugg_debt = sugg_payment_info[0]
 		# print "This is suggested debt on my card as it decreases:", sugg_debt
-		total_sugg_debt.append(sugg_debt)
-		# print "Total suggested debt decreasing", total_sugg_debt
+		sugg_debt_list.append(sugg_debt)
 
-		sugg_int = sugg_info[1]
+		sugg_int = sugg_payment_info[1]
 		# print "This is the suggested interest accruing for each payment:", sugg_int
-		total_sugg_int.append(sugg_int)
-		# print "sugg int", total_sugg_int
+		sugg_int_list.append(sugg_int)
 
-		sugg_payment = sugg_info[2]
+		sugg_payment = sugg_payment_info[2]
 		# print "This is the suggested payments for card until debt is gone:", sugg_payment
+		sugg_payment_list.append(sugg_payment)
 
 
-		total_sugg_payment.append(sugg_payment)
-		# print "sugg payment", total_sugg_payment
-
-	total_min_debt = [x + y for x, y in zip(total_min_debt[0], total_min_debt[1])]
-	rounded_total_min_debt = [round(x, 2) for x in total_min_debt]
-
-	
-	total_min_int = [x + y for x, y in zip(total_min_int[0], total_min_int[1])]
-	rounded_total_min_int = [round(x, 2) for x in total_min_int]
-
-	
-	total_min_payment = [x + y for x, y in zip(total_min_payment[0], total_min_payment[1])]
-	rounded_total_min_payment = [round(x, 2) for x in total_min_payment]
-
-	
-	total_sugg_debt = [x + y for x, y in zip(total_sugg_debt[0], total_sugg_debt[1])]
-	rounded_total_sugg_debt = [round(x, 2) for x in total_sugg_debt]
-	# print "total rounded sugg debt", rounded_total_sugg_debt
-
-	
-	total_sugg_int = [x + y for x, y in zip(total_sugg_int[0], total_sugg_int[1])]
-	rounded_total_sugg_int = [round(x, 2) for x in total_sugg_int]
+# # WORKING TOTAL LISTS FROM NEW REVAMP OF CODE
+# 	print "This is my total min debt list for all cards", min_debt_list
+# 	print "This is my total min int for all cards", min_int_list
+# 	print "This is my total min payment for all cards", min_payment_list
+	# print "Total suggested debt decreasing", sugg_debt_list
+	# print "Total sugg int", sugg_int_list
+	# print "Total sugg payment", sugg_payment_list
 
 
-	total_sugg_payment = [x + y for x, y in zip(total_sugg_payment[0], total_sugg_payment[1])]
-	rounded_total_sugg_payment = [round(x, 2) for x in total_sugg_payment]
+	total_min_debt = zip(*min_debt_list)
+	# print "This is my min debt zipped list", total_min_debt
+	total_min_int = zip(*min_int_list)
+	# print "This is my min int zipped list", total_min_int
+	total_min_payment = zip(*min_payment_list)
+	# print "This is my min payment zipped list", total_min_payment
 
+	total_sugg_debt = zip(*sugg_debt_list)
+	# print "This is my sugg debt zipped list", total_sugg_debt
+	total_sugg_int = zip(*sugg_int_list)
+	# print "This is my sugg int zipped list", total_sugg_int
+	total_sugg_payment = zip(*sugg_payment_list)
+	# print "This is my sugg payment zipped list", total_sugg_payment
 
-	# rounded_all_totals = [float(int(x)) for x in all_totals]
-	# print "total sugg payment", total_sugg_payment
-
-	# print "total min debt:", total_min_debt
-	# print '*' * 30
-	# print "total min int:",  total_min_int
-	# print '*' * 30
-	# print "total min payment", total_min_payment
-	# print '*' * 30
-	# print "total sugg debt:", total_sugg_debt
-	# print '*' * 30
-	# print "total sugg int", total_sugg_int
-	# print '*' * 30
-	# print "total sugg payment", total_sugg_payment
-
+# Maybe making the choice here NOT to round out numbers. Will round in Jinja on HTML.
 
 	now = datetime.now()
-	dt_min_month = pd.date_range(datetime.strftime(now, '%Y-%m-%d'), periods=len(rounded_total_min_debt), freq='M')
-	
-	dt_sugg_month = pd.date_range(datetime.strftime(now, '%Y-%m-%d'), periods=len(rounded_total_sugg_debt), freq='M')
+	dt_min_month = pd.date_range(datetime.strftime(now, '%Y-%m-%d'), periods=len(total_min_debt), freq='M')	
+	dt_sugg_month = pd.date_range(datetime.strftime(now, '%Y-%m-%d'), periods=len(total_sugg_debt), freq='M')
 
-	# How to get month and year to display on table?
-	total_min = zip(*[dt_min_month, rounded_total_min_debt, rounded_total_min_int, rounded_total_min_payment])
-	total_sugg = zip(*[dt_sugg_month, rounded_total_sugg_debt, rounded_total_sugg_int, rounded_total_sugg_payment])
+
+	total_calc_min_debt = [] 
+	for i in total_min_debt:
+		sum_of = 0
+		for x in i:
+			sum_of = sum_of + x
+		total_calc_min_debt.append(sum_of)
+
+
+	total_calc_min_int = []
+	for i in total_min_int:
+		sum_of = 0
+		for x in i:
+			sum_of = sum_of + x
+		total_calc_min_int.append(sum_of)
+
+	total_calc_min_payment = []
+	for i in total_min_payment:
+		sum_of = 0
+		for x in i:
+			sum_of = sum_of + x
+		total_calc_min_payment.append(sum_of)
+
+
+	total_calc_sugg_debt = []
+	for i in total_sugg_debt:
+		sum_of = 0
+		for x in i:
+			sum_of = sum_of + x
+		total_calc_sugg_debt.append(sum_of)
+
+
+	total_calc_sugg_int = []
+	for i in total_sugg_int:
+		sum_of = 0
+		for x in i:
+			sum_of = sum_of + x
+		total_calc_sugg_int.append(sum_of)
+
+
+	total_calc_sugg_payment = []
+	for i in total_sugg_payment:
+		sum_of = 0
+		for x in i:
+			sum_of = sum_of + x
+		total_calc_sugg_payment.append(sum_of)
+
+	total_min = zip(*[dt_min_month, total_calc_min_debt, total_calc_min_int, total_calc_min_payment])
+	total_sugg = zip(*[dt_sugg_month, total_calc_sugg_debt, total_calc_sugg_int, total_calc_sugg_payment])
+	# print "This is total sugg - (date, debt, int, payment )", total_sugg
+
+#I don't think I need to have the same values for both min and sugg so we shall see...
 	# print "M:", len(total_min)
 	# print "S:", len(total_sugg)
 
 
-	# all_totals = [total_min, total_sugg]
-	
+
+	while len(total_sugg_debt) < len(total_min_debt):
+		total_sugg_debt.append((0, 0, 0))
+
+
+	total_calc_sugg_debt = []
+	for i in total_sugg_debt:
+		sum_of = 0
+		for x in i:
+			sum_of = sum_of + x
+		total_calc_sugg_debt.append(sum_of)
+
+
+	all_totals = [total_min, total_sugg]
+
+# I think these are right......
 	df_min = pd.DataFrame(data = total_min, columns=['Month', 'Debt', 'Interest', 'Payments'])
+	# print "This is my Min panda", df_min
 	df_sugg = pd.DataFrame(data = total_sugg, columns=['Month', 'Debt', 'Interest', 'Payments'])
 
-	#**************# A debugger tool from Rachael: #**************#
-	# import pdb; pdb.set_trace()
-
-#********** This is the list of my data points (date: ##, Min: ##, Sugg: ##) that will be JSONified and passed to D3 **********#
-	min_point = zip(*[dt_min_month.date, rounded_total_min_debt])
-	# print "Min length:", len(min_point)
-	# print "This is zipped point", min_point
-
-	while len(rounded_total_sugg_debt) < len(rounded_total_min_debt):
-		rounded_total_sugg_debt.append(0)
 
 
-	new_data_points = zip(*[dt_min_month.date, rounded_total_min_debt, rounded_total_sugg_debt])
+# 	#**************# A debugger tool from Rachael: #**************#
+# 	# import pdb; pdb.set_trace()
 
+
+
+
+# #********** This is the list of my data points (date: ##, Min: ##, Sugg: ##) that will be JSONified and passed to D3 **********#
+	new_data_points = zip(*[dt_min_month.date, total_calc_min_debt, total_calc_sugg_debt])
 
 	new_data_point_list = []
 	for i in range(len(new_data_points)):
